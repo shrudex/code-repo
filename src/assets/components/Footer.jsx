47037ffe3446d872eee37403f8db115d5e2df3b7
shrudex <codeshubh@gmail.com>
2023-09-17 21:06:52 +0530

    footer width

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className=" bg-white shadow dark:bg-gray-900 relative top-8 w-full h-auto">
+    <footer className=" bg-white shadow dark:bg-gray-900 relative top-8 w-full h-60 mb-9">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <NavLink
